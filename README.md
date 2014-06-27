62f4354aaf832e3c69b309babd40a041f278b45b
Remco Tolsma <info@remcotolsma.nl>
2014-06-27 17:17:33 +0200

    Init commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# WordPress Pay Gateway: Qantani
+
+**iDEAL Basic driver for the WordPress payment processing library.**
+
+[![Build Status](https://travis-ci.org/wp-pay-gateways/qantani.svg?branch=develop)](https://travis-ci.org/wp-pay-gateways/qantani)
+[![Coverage Status](https://coveralls.io/repos/wp-pay-gateways/qantani/badge.png?branch=develop)](https://coveralls.io/r/wp-pay-gateways/qantani?branch=develop)
---
+++ tests/ErrorTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Title: Qantani error test
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Gateways_Qantani_ErrorTest extends PHPUnit_Framework_TestCase {
+	public function testToStringError() {
+		$error = new Pronamic_WP_Gateways_Qantani_Error( '1', 'Error' );
+
+		$string = (string) $error;
+
+		$expected = '1 Error';
+
+		$this->assertEquals( $expected, $string );
+	}
+}
