fbdbf447472f6575386f3fb813f7f65513401193
Remco Tolsma <info@remcotolsma.nl>
2015-03-03 14:11:42 +0100

    Set copyright year to 2015.

--- src/Gateway.php
+++ src/Gateway.php
@@ -3,10 +3,10 @@
 /**
  * Title: Qantani gateway
  * Description:
- * Copyright: Copyright (c) 2005 - 2011
+ * Copyright: Copyright (c) 2005 - 2015
  * Company: Pronamic
  * @author Remco Tolsma
- * @version 1.0
+ * @version 1.0.0
  */
 class Pronamic_WP_Pay_Gateways_Qantani_Gateway extends Pronamic_WP_Pay_Gateway {
 	/**
