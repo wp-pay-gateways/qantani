5135ca85463290a9f328db796668af82dc14bdc3
Reüel van der Steege <reuel@pronamic.nl>
2016-03-03 16:32:25 +0100

    Update gateway settings

--- src/Settings.php
+++ src/Settings.php
@@ -21,6 +21,7 @@ class Pronamic_WP_Pay_Gateways_Qantani_Settings extends Pronamic_WP_Pay_GatewayS
 		$sections['qantani'] = array(
 			'title'   => __( 'Qantani', 'pronamic_ideal' ),
 			'methods' => array( 'qantani' ),
+			'description' => __( 'Account details are provided by the payment provider after registration. These settings need to match with the payment provider dashboard.', 'pronamic_ideal' ),
 		);
 
 		return $sections;
@@ -35,6 +36,14 @@ class Pronamic_WP_Pay_Gateways_Qantani_Settings extends Pronamic_WP_Pay_GatewayS
 			'title'       => _x( 'Merchant ID', 'qantani', 'pronamic_ideal' ),
 			'type'        => 'text',
 			'classes'     => array( 'code' ),
+			'tooltip'     => sprintf(
+				'%s %s.',
+				_x( 'Merchant ID', 'qantani', 'pronamic_ideal' ),
+				sprintf(
+					__( 'as mentioned in the %s dashboard', 'pronamic_ideal' ),
+					__( 'Qantani', 'pronamic_ideal' )
+				)
+			),
 		);
 
 		// Merchant Key
@@ -45,6 +54,14 @@ class Pronamic_WP_Pay_Gateways_Qantani_Settings extends Pronamic_WP_Pay_GatewayS
 			'title'       => _x( 'Merchant Key', 'qantani', 'pronamic_ideal' ),
 			'type'        => 'text',
 			'classes'     => array( 'code' ),
+			'tooltip'     => sprintf(
+				'%s %s.',
+				_x( 'Merchant Key', 'qantani', 'pronamic_ideal' ),
+				sprintf(
+					__( 'as mentioned in the %s dashboard', 'pronamic_ideal' ),
+					__( 'Qantani', 'pronamic_ideal' )
+				)
+			),
 		);
 
 		// Merchant Secret
@@ -55,6 +72,25 @@ class Pronamic_WP_Pay_Gateways_Qantani_Settings extends Pronamic_WP_Pay_GatewayS
 			'title'       => _x( 'Merchant Secret', 'qantani', 'pronamic_ideal' ),
 			'type'        => 'text',
 			'classes'     => array( 'regular-text', 'code' ),
+			'tooltip'     => sprintf(
+				'%s %s.',
+				_x( 'Merchant Secret', 'qantani', 'pronamic_ideal' ),
+				sprintf(
+					__( 'as mentioned in the %s dashboard', 'pronamic_ideal' ),
+					__( 'Qantani', 'pronamic_ideal' )
+				)
+			),
+		);
+
+		// Transaction feedback
+		$fields[] = array(
+			'section'       => 'qantani',
+			'title'         => __( 'Transaction feedback', 'pronamic_ideal' ),
+			'type'          => 'description',
+			'html'          => sprintf(
+				'<span class="dashicons dashicons-no pronamic-pay-no"></span> %s',
+				__( 'Payment status updates are not supported by this payment provider.', 'pronamic_ideal' )
+			),
 		);
 
 		return $fields;
